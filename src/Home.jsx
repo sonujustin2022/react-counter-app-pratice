ed31b28c3ab6c485efbd839372e88c07d63f1b0a
sonujustin2022 <sonujustin6282@gmail.com>
2026-04-28 12:05:19 +0530

    counter finished

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,7 +2,10 @@ import React, { useState } from 'react'
 
 const Home = () => {
 
-const [value,setValue] = useState('0') 
+const [value,setValue] = useState(0) ;
+const [inpValue,setInpValue] = useState(0);
+
+
 
   return (
 <div className='theBody'>
@@ -10,9 +13,13 @@ const [value,setValue] = useState('0')
     <div className='counterApp'>
         <div><h1> {value}</h1></div>
         <div>
-        <button>+</button>
-        <button>reset</button>
-        <button>-</button>
+        <button onClick={()=>setValue(value+1)}>+</button>
+        <button onClick={()=>setValue(0)}>reset</button>
+        <button onClick={()=>setValue(value-1)}>-</button>
+    </div>
+    <div>
+        <input onChange={(event)=>setInpValue(event.target.value)} type="number"  placeholder='enter value to add or sub'/>
+        <button onClick={()=>setValue(Number(value)+Number(inpValue))}>add this value</button>
     </div>
     </div>
 </div>
